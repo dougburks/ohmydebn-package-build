12c49800d89a66965418a548ab12e7bde284b3a6
Doug Burks <doug.burks@securityonionsolutions.com>
2025-11-18 15:54:39 -0500

    update package scripts

---
+++ upload-to-repo-stable.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ohmydebn-packages
+
+for PACKAGE in mint-themes mint-x-icons ohmydebn-aether ohmydebn-gtile ohmydebn-themes-omarchy ohmydebn-themes ohmydebn; do
+  reprepro remove trixie ${PACKAGE}
+  reprepro -b . includedeb trixie ../${PACKAGE}_*
+done
+
+git add -A
+git commit -m "update packages"
+git push
+
+cd - >/dev/null
