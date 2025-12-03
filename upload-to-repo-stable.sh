672f5fa83c0b283259c4f6e5252dc1135be61d2d
Doug Burks <doug.burks@securityonionsolutions.com>
2025-12-03 09:22:26 -0500

    update for new font packages

--- upload-to-repo-stable.sh
+++ upload-to-repo-stable.sh
@@ -2,7 +2,16 @@
 
 cd ohmydebn-packages
 
-for PACKAGE in mint-themes mint-x-icons ohmydebn-aether ohmydebn-caskaydiamononerdfont ohmydebn-gtile ohmydebn-themes-omarchy ohmydebn-themes ohmydebn; do
+for PACKAGE in mint-themes \
+  mint-x-icons \
+  ohmydebn-aether \
+  ohmydebn-caskaydiamononerdfont \
+  ohmydebn-caskaydiamononerdfontmono \
+  ohmydebn-caskaydiamononerdfontpropo \
+  ohmydebn-gtile \
+  ohmydebn-themes-omarchy \
+  ohmydebn-themes \
+  ohmydebn; do
   reprepro remove trixie ${PACKAGE}
   reprepro -b . includedeb trixie ../${PACKAGE}_*
 done
