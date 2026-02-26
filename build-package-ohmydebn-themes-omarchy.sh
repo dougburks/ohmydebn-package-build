e269821b3a7242c5f5f50c9b6d9a8388a71b2791
Doug Burks <doug.burks@securityonionsolutions.com>
2026-02-26 16:51:56 -0500

    create individual packages for each omarchy theme

--- build-package-ohmydebn-themes-omarchy.sh
+++ build-package-ohmydebn-themes-omarchy.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
 
-PACKAGE="ohmydebn-themes-omarchy"
-VERSION="3.3.1"
+VERSION=$(cat ~/git/omarchy/version)
+PREFIX="ohmydebn-themes"
+
+# Create individual package for each omarchy theme
+for THEME in catppuccin catppuccin-latte ethereal everforest flexoki-light gruvbox hackerman kanagawa matte-black miasma nord osaka-jade ristretto rose-pine tokyo-night vantablack white; do
+
+  PACKAGE="${PREFIX}-${THEME}"
+  rm -f ${PACKAGE}_*.deb
+
+  fpm -s dir \
+    -t deb \
+    -n ${PACKAGE} \
+    -v ${VERSION} \
+    -a all \
+    --maintainer "Doug Burks<doug.burks@example.com>" \
+    --description "${THEME} theme from Omarchy packaged for OhMyDebn" \
+    --url "https://ohmydebn.org" \
+    ~/git/omarchy/themes/${THEME}=/usr/share/ohmydebn-themes/
+
+  echo
+  ls -alh ${PACKAGE}_*.deb
+  echo
+  cd ohmydebn-packages-testing
+  reprepro remove trixie ${PACKAGE}
+  reprepro -b . includedeb trixie ../${PACKAGE}_${VERSION}_all.deb
+  cd - >/dev/null
+
+done
+
+# Create the ohmydebn-themes-omarchy package that just contains the version file
+PACKAGE="${PREFIX}-omarchy"
 rm -f ${PACKAGE}_*.deb
 
-fpm -s dir \
+fpm -s empty \
   -t deb \
   -n ${PACKAGE} \
   -v ${VERSION} \
@@ -12,7 +41,23 @@ fpm -s dir \
   --maintainer "Doug Burks<doug.burks@example.com>" \
   --description "Themes from Omarchy packaged for OhMyDebn" \
   --url "https://ohmydebn.org" \
-  ~/git/omarchy/themes/=/usr/share/${PACKAGE}
+  -d ${PREFIX}-catppuccin \
+  -d ${PREFIX}-catppuccin-latte \
+  -d ${PREFIX}-ethereal \
+  -d ${PREFIX}-everforest \
+  -d ${PREFIX}-flexoki-light \
+  -d ${PREFIX}-gruvbox \
+  -d ${PREFIX}-hackerman \
+  -d ${PREFIX}-kanagawa \
+  -d ${PREFIX}-matte-black \
+  -d ${PREFIX}-miasma \
+  -d ${PREFIX}-nord \
+  -d ${PREFIX}-osaka-jade \
+  -d ${PREFIX}-ristretto \
+  -d ${PREFIX}-rose-pine \
+  -d ${PREFIX}-tokyo-night \
+  -d ${PREFIX}-vantablack \
+  -d ${PREFIX}-white
 
 echo
 ls -alh ${PACKAGE}_*.deb
