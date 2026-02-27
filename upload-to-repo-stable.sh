04db610f7ba77052e69ca8d3c4a7576056acf0b8
Doug Burks <doug.burks@securityonionsolutions.com>
2026-02-27 07:51:39 -0500

    update upload-to-repo-stable.sh to include new theme packages

--- upload-to-repo-stable.sh
+++ upload-to-repo-stable.sh
@@ -10,7 +10,24 @@ for PACKAGE in mint-themes \
   ohmydebn-caskaydiamononerdfontpropo \
   ohmydebn-gtile \
   ohmydebn-templates \
+  ohmydebn-themes-catppuccin-latte \
+  ohmydebn-themes-catppuccin \
+  ohmydebn-themes-ethereal \
+  ohmydebn-themes-everforest \
+  ohmydebn-themes-flexoki-light \
+  ohmydebn-themes-gruvbox \
+  ohmydebn-themes-hackerman \
+  ohmydebn-themes-kanagawa \
+  ohmydebn-themes-matte-black \
+  ohmydebn-themes-miasma \
+  ohmydebn-themes-nord \
   ohmydebn-themes-omarchy \
+  ohmydebn-themes-osaka-jade \
+  ohmydebn-themes-ristretto \
+  ohmydebn-themes-rose-pine \
+  ohmydebn-themes-tokyo-night \
+  ohmydebn-themes-vantablack \
+  ohmydebn-themes-white \
   ohmydebn-themes \
   ohmydebn; do
   reprepro remove trixie ${PACKAGE}
