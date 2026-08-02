287b608c44eb79fa9c0897db292f184878db9b31
Doug Burks <doug.burks@securityonionsolutions.com>
2026-08-02 07:28:46 -0400

    4.1.0

--- build-package-mint.sh
+++ build-package-mint.sh
@@ -4,16 +4,22 @@
 # which conflicts with the newer mint-cursor-themes
 # so we need to build a new mint-cursor-themes package
 # that has the --conflicts and --replaces flags like this:
+#
 # fpm -s deb -t deb \
 #  -n mint-cursor-themes \
-#  -v 1.0.2-ohmydebn1 \
+#  -v 1.0.2-ohmydebn2 \
 #  --conflicts bibata-cursor-theme \
 #  --replaces bibata-cursor-theme \
+#  --deb-compression xz \
+#  --deb-compression-level 9 \
 #  mint-cursor-themes_1.0.2_all.deb
+#
+# Also note that the --deb-compression flags are required to
+# keep the package size from becoming much bigger.
 
 cd ohmydebn-packages-testing
 reprepro remove trixie mint-cursor-themes
-reprepro -b . includedeb trixie ../mint-cursor-themes_1.0.2-ohmydebn1_all.deb
+reprepro -b . includedeb trixie ../mint-cursor-themes_1.0.2-ohmydebn2_all.deb
 reprepro remove trixie mint-themes
 reprepro -b . includedeb trixie ../mint-themes_2.3.8_all.deb
 reprepro remove trixie mint-x-icons
